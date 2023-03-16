657a1eee7204cdf71eae2f96de9a37ab9e21931d
JANAGAN R <102809122+JANAGAN71@users.noreply.github.com>
2023-03-16 21:59:10 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -10,7 +10,7 @@ $prf_collection=$client->selectCollection($prf_db,'prf_collection');
 if(isset($_POST['submit'])){
     $userData=[
         $firstname = $_POST['firstname'],
-        $lastname= $_POST['Lastname'],
+        $lastname= $_POST['lastname'],
         $Mobile_Number = $_POST['Mobile_Number'],
         $DOB= $_POST['DOB'],
         $country = $_POST['country'],
